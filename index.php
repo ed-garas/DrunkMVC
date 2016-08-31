e81a382f28b0cb4a710ea0f031366043fa78abc2
ed <ed@robotboyz.com>
2016-08-31 17:16:04 +0300

    ed

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include 'View.php';
 $layout = new View('templates/layout.php');
 $meta = new View('templates/meta.php', array('title'=>'Babina'));
+$url = new View('templates/meta.php', array('title'=>'Babina'));
 
 $layout->addData('arr', array('vienas',2, 'trys'));
 $layout->addData('coo', true);
